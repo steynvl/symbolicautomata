a7b8d1a7d7b19d86f665a2f1d60116efe38d18a1
steynvl <steyn.van.l@gmail.com>
2020-01-16 18:01:55 +0200

    Tests skeleton for SAFA

---
+++ SVPABenchmark/src/benchmark/regexconverter/TestRegexTranslation.java
@@ -0,0 +1,13 @@
+package benchmark.regexconverter;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestRegexTranslation {
+
+    @Test
+    public void test01() {
+        assertTrue(true);
+    }
+
+}
---
+++ SVPABenchmark/src/benchmark/regexconverter/TestSAFA.java
@@ -0,0 +1,12 @@
+package benchmark.regexconverter;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    TestRegexTranslation.class
+})
+
+public class TestSAFA { }
